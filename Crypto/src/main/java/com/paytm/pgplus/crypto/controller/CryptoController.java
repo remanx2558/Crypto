dd0415f50724c8757f4e7c26720a61d17d4ffdf5
anshupriya <anshu.priya@paytm.com>
2021-10-21 23:59:10 +0530

    created block

--- Crypto/src/main/java/com/paytm/pgplus/crypto/controller/CryptoController.java
+++ Crypto/src/main/java/com/paytm/pgplus/crypto/controller/CryptoController.java
@@ -1,4 +1,24 @@
 package com.paytm.pgplus.crypto.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CryptoController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CryptoController.class);
+
+    @GetMapping("/")
+    public String routeDefault(){
+        return  "Welcome to the Crypto World";
+    }
+
+    @GetMapping("/blockchain")
+    public String routeBlockchain(){
+        return "xyz";
+        //return jsonify(blockchain.to_json());
+    }
+
 }
