16d0d0dc2457a45c3769ab6e88dc8e1130b051c2
anshupriya <anshu.priya@paytm.com>
2021-10-22 17:24:01 +0530

    solved

---
+++ Crypto/src/main/java/com/paytm/pgplus/crypto/blockchain/BlockChain.java
@@ -0,0 +1,41 @@
+package com.paytm.pgplus.crypto.blockchain;
+
+
+import com.paytm.pgplus.crypto.blockchain.blockActivity.BlockActs;
+
+import lombok.Data;
+
+
+import java.util.ArrayList;
+
+
+@Data
+
+public class BlockChain {
+
+    //  Blockchain: a public ledger of transactions.
+
+    //    Implemented as a list of blocks - data sets of transactions
+
+
+
+    ArrayList<Block>chain;
+
+    public BlockChain(){
+
+        chain=new ArrayList<>();
+
+    }
+
+    public void add_block(DataBlock data){
+
+        Block last_Block=(chain.size()>0)?chain.get(chain.size()-1):null;
+
+
+        chain.add(BlockActs.mine_block(last_Block,data));
+
+        //System.out.println(String.valueOf(this));
+
+    }
+
+}
