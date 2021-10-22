e234f95f571e2d0b3553175e3196703236c53b54
Suraj Kumar Patel <suraj1.patel@paytm.com>
2021-10-22 15:42:59 +0530

    Transaction class is updated and related changes

--- Crypto/src/main/java/com/paytm/pgplus/crypto/wallet/TransactionPool.java
+++ Crypto/src/main/java/com/paytm/pgplus/crypto/wallet/TransactionPool.java
@@ -1,4 +1,16 @@
 package com.paytm.pgplus.crypto.wallet;
 
+import java.util.HashMap;
+
 public class TransactionPool {
+
+
+    public TransactionPool() {
+
+    }
+
+    public void setTransaction(Transaction transaction){
+
+    }
+
 }
